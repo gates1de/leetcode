86e88aad3941e0941dc050624c255d6fc050ed0b
gates1de <g031i043@gmail.com>
2021-01-06 21:06:06 +0900

    2021-week1-3951: Passed n = 1 ~ 6

--- 2021/week1/3591/main.go
+++ 2021/week1/3591/main.go
@@ -5,21 +5,56 @@ import (
 
 func countArrangement(n int) int {
 	result := 0
-	if n <= 3 {
-		return n
+	nums := make([]int, n)
+	for i := 1; i <= n; i++ {
+		nums[i - 1] = i
 	}
 
-	// perm := make([]int, n)
-	// for i := 1; i < n; i++ {
-	// 	for j := 1; j < n; j++ {
-	// 	}
-	// }
-	// fmt.Printf("perm = %v\n", perm)
+    init := make([][]int, 0, n)
+	perm := generatePermute(nums, []int{}, init)
+
+TOP:
+	for _, p := range perm {
+		// fmt.Printf("perm = %v\n", p)
+		for i := 1; i <= len(p); i++ {
+			p_val := p[i - 1]
+			if (i % p_val) != 0 && (p_val % i) != 0 {
+				continue TOP
+			}
+		}
+		// fmt.Printf("beatiful arrangement = %v\n", p)
+		result++
+	}
 	return result
 }
 
+func copySlice(target []int) []int {
+    result := make([]int, len(target))
+    copy(result, target)
+    return result
+}
+
+func generatePermute(nums []int, perm []int, current [][]int) [][]int {
+	n := len(nums)
+
+	if n == 0 {
+		// fmt.Printf("added perm = %v\n", perm)
+		current = append(current, perm)
+		return current
+	}
+
+	for i := 0; i < n; i++ {
+		new_nums := make([]int, 0, n)
+		new_nums = append(new_nums, nums[:i]...)
+		new_nums = append(new_nums, nums[i + 1:]...)
+		current = generatePermute(new_nums, append(perm, nums[i]), current)
+	}
+	return current
+}
+
 func main() {
-	result := countArrangement(1)
-	fmt.Printf("result = %v\n", result)
+	n := 6
+	r := countArrangement(n)
+	fmt.Printf("result = %v\n", r)
 }
 
