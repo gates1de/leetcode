c7657da8320fca56333f22caf75df9bbff20a3e5
gates1de <g031i043@gmail.com>
2021-01-13 11:47:05 +0900

    2021-week1-3595: Accepted

--- 2021/week1/3595/main.go
+++ 2021/week1/3595/main.go
@@ -9,63 +9,30 @@ func lengthOfLongestSubstring(s string) int {
 		return 0
 	}
 
-	splitS := split(s)
-	longestIndex := int(0)
-	for i := 0; i < len(splitS); i++ {
-		substring := splitS[i]
-		if len(substring) >= len(splitS[longestIndex]) {
-			longestIndex = i
-		}
-	}
-
-	longestSubstring := splitS[longestIndex]
-    // fmt.Printf("longestSubstring = %v\n", longestSubstring)
-	result := len(longestSubstring)
-	if longestIndex == 0 {
-		return result
-	} else {
-		preSubstring := splitS[longestIndex - 1]
-		psCharacters := strings.Split(preSubstring, "")
-		// fmt.Printf("preSubstring = %v\n", preSubstring)
-		for i := len(psCharacters) - 1; i >= 0; i-- {
-			c := psCharacters[i]
-			// fmt.Printf("c = %v\n", c)
-			if strings.Contains(longestSubstring, c) {
-				break
-			}
-			result++
-		}
-	}
-	return result
-}
-
-func split(s string) []string {
-	result := []string{}
-
-	resultIndex := int(0)
+	result := int(0)
 	characters := strings.Split(s, "")
+	substring := ""
 	for i := 0; i < len(characters); i++ {
 		c := characters[i]
-		if len(result) == 0 {
-			result = append(result, c)
-			continue
-		}
-		substring := result[resultIndex]
-		if strings.Contains(substring, c) {
-			result = append(result, c)
-			resultIndex++
+		cIndex := strings.Index(substring, c)
+		if cIndex >= 0 {
+			substring = substring[cIndex + 1:] + c
 		} else {
-			result[resultIndex] = substring + c
+			substring += c
 		}
-		// fmt.Printf("result = %v\n", result)
+
+		if result < len(substring) {
+			result = len(substring)
+		}
 	}
 	return result
 }
 
 func main() {
 	// s := "abcabcbb"
-	// s := "pwwkew"
-	s := "dvdf"
+    // s := "pwwkew"
+	// s := "dvdf"
+	s := "anviaj"
 	result := lengthOfLongestSubstring(s)
 	fmt.Printf("result = %v\n", result)
 }
