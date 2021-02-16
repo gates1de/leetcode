ad733c90f9e790260b54c29db5150157ac11cf80
gates1de <g031i043@gmail.com>
2021-02-16 10:30:07 +0900

    2021-week4-3619: Accepted

--- 2021/week4/3619/main.go
+++ 2021/week4/3619/main.go
@@ -1,29 +1,35 @@
 package main
 import (
 	"fmt"
+	"strings"
 )
 
 func getSmallestString(n int, k int) string {
 	result := ""
 	for k > 0 {
 		if n == k {
-			for i := 0; i < n; i++ {
-				result = "a" + result
-			}
+			result = strings.Repeat("a", n) + result
 			break
 		}
 
-		if k - n < 26 {
-			c := string(rune(97 + k - n))
-			// fmt.Printf("c = %v\n", c)
-			result = c + result
-			n--
-			k = n
+		if k - n >= 26 {
+			zCount := (k - n) / 26
+			// fmt.Printf("zCount = %v\n", zCount)
+			result += strings.Repeat("z", zCount)
+			k -= 26 * zCount
+			n -= zCount
+			if n >= k {
+				k += 26
+				n++
+			}
 			continue
 		}
-		result += "z"
-		k -= 26
+
+		c := string(rune(97 + k - n))
+		// fmt.Printf("c = %v, 97 + k - n = %v\n", c, 97 + k - n)
+		result = c + result
 		n--
+		k = n
 	}
 	return result
 }
@@ -34,6 +40,9 @@ func main() {
 
 	// n := int(5)
 	// k := int(73)
+
+	// n := int(85)
+	// k := int(2159)
 
 	n := int(91713)
 	k := int(116185)
