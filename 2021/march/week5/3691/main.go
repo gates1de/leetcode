71fbfa42f7bd1e7129117d9a99f89332cdbf2a21
gates1de <g031i043@gmail.com>
2021-04-01 19:53:59 +0900

    2021-march-week5-3691: Accepted

--- 2021/march/week5/3691/main.go
+++ 2021/march/week5/3691/main.go
@@ -5,6 +5,58 @@ import (
 )
 
 func movesToStamp(stamp string, target string) []int {
+	letters := []byte(target)
+	toErase := len(target)
+
+	contains := func(offset int) bool {
+		empty := true
+		for i := 0; i < len(stamp); i++ {
+			if l := letters[offset + i]; l != '?' && l != stamp[i] {
+				return false
+			} else if l != '?' {
+				empty = false
+			}
+		}
+		return !empty
+	}
+
+	erase := func(offset int) {
+		for i := 0; i < len(stamp); i++ {
+			if letters[offset + i] != '?' {
+				letters[offset + i] = '?'
+				toErase--
+			}
+		}
+	}
+
+	var result []int
+	for toErase != 0 {
+		erased := false
+		for i := 0; i < len(letters) - len(stamp) + 1; i++ {
+			if contains(i) {
+				erase(i)
+				result = append(result, i)
+				erased = true
+				i += len(stamp)
+				if toErase == 0 {
+					break
+				}
+			}
+		}
+		if !erased {
+			return []int{}
+		}
+	}
+
+	for l, r := 0, len(result) - 1; l < r; l, r = l + 1, r - 1 {
+		result[l], result[r] = result[r], result[l]
+	}
+
+	return result
+}
+
+// Wrong Answer
+func ngSolution(stamp string, target string) []int {
 	result := []int{}
 	for _, sRune := range stamp {
 		if !strings.Contains(target, string(sRune)) {
