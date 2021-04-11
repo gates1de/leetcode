85d073f15072464ca90ddef64ad06572332df866
gates1de <g031i043@gmail.com>
2021-04-11 16:21:07 +0900

    2021-april-week2-3703: Accepted

--- 2021/april/week2/3703/main.go
+++ 2021/april/week2/3703/main.go
@@ -6,6 +6,62 @@ import (
 )
 
 func longestIncreasingPath(matrix [][]int) int {
+    if len(matrix) == 0 || len(matrix[0]) == 0 {
+        return 0
+    }
+
+    m := len(matrix)
+    n := len(matrix[0])
+
+    memo := make([][]int, m)
+    for i := 0; i < m; i++ {
+        memo[i] = make([]int, n)
+    }
+
+    result := 1
+    for i := 0; i < m; i++ {
+        for j := 0; j < n; j++ {
+            result = max(result, helper(matrix, i, j, memo))
+        }
+    }
+    return result
+}
+
+func helper(matrix [][]int, i int, j int, memo [][]int) int {
+    if memo[i][j] != 0 {
+        return memo[i][j]
+    }
+
+    m := len(matrix)
+    n := len(matrix[0])
+
+    result := 1
+	// left
+    if i - 1 >= 0 && matrix[i - 1][j] > matrix[i][j] {
+        result = max(result,  1 + helper(matrix, i - 1, j, memo))
+    }
+
+	// right
+    if i + 1 < m && matrix[i + 1][j] > matrix[i][j] {
+        result = max(result, 1 + helper(matrix, i + 1, j, memo))
+    }
+
+	// up
+    if j - 1 >= 0 && matrix[i][j - 1] > matrix[i][j] {
+        result = max(result, 1 + helper(matrix, i, j - 1, memo))
+    }
+
+	// down
+    if j + 1 < n && matrix[i][j + 1] > matrix[i][j] {
+        result = max(result, 1 + helper(matrix, i, j + 1, memo))
+    }
+
+    memo[i][j] = result
+    return result
+}
+
+// Time Limit Exceeded
+func ngSolution(matrix [][]int) int {
 	if len(matrix) == 0 {
 		return 0
 	} else if len(matrix) == 1 && len(matrix[0]) == 1 {
@@ -27,14 +83,10 @@ func longestIncreasingPath(matrix [][]int) int {
 	count := int(0)
 	for h.Len() > 0 {
 		top := heap.Pop(h).([]int)
-		fmt.Printf("top = %v, current matrix = %v\n", top, matrix[top[0]][top[1]])
-		// if visited[top[0]][top[1]] {
-		// 	continue
-		// }
+		// fmt.Printf("top = %v, current matrix = %v\n", top, matrix[top[0]][top[1]])
+
 		visited[top[0]][top[1]] = true
-		// if top[0] == maxY && top[1] == maxX {
-		// 	return top[2]
-        // }
+
 		for _, d := range dir {
 			newY, newX := top[0] + d[0], top[1] + d[1]
 			if (newX < 0 || maxX < newX) || (newY < 0 || maxY < newY) || (matrix[newY][newX] <= matrix[top[0]][top[1]]) {
@@ -44,16 +96,9 @@ func longestIncreasingPath(matrix [][]int) int {
 			if count < top[2] + 1 {
 				count = top[2] + 1
 			}
-			// heap.Push(h, []int{
-			// 	newY, newX, max(
-			// 		top[2],
-			// 		abs(matrix[newY][newX], matrix[top[0]][top[1]]),
-			// 	),
-			// })
 		}
 
 		if h.Len() == 0 {
-			fmt.Printf("heap = %v\n", h)
 			TOP: for i, v := range visited {
 				for j, isVisited := range v {
 					if !isVisited {
@@ -64,7 +109,6 @@ func longestIncreasingPath(matrix [][]int) int {
 			}
 		}
 	}
-	fmt.Printf("visited = %v\n", visited)
 	return count + 1
 }
 
@@ -90,13 +134,10 @@ func main() {
 	// matrix := [][]int{{3, 4, 5}, {3, 2, 6}, {2, 2, 1}}
 
 	// result: 1
-	matrix := [][]int{{1}}
+	// matrix := [][]int{{1}}
 
 	// result: 2 ( path -> [1, 2] )
-	// matrix := [][]int{{1, 2}}
-
-	// result:
-	// matrix := [][]int{}
+	matrix := [][]int{{1, 2}}
 
 	result := longestIncreasingPath(matrix)
 	fmt.Printf("result = %v\n", result)
