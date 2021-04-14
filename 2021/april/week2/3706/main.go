e56c3f30a0f99272f08aa9619126feff72eac29a
gates1de <g031i043@gmail.com>
2021-04-14 16:05:58 +0900

    2021-april-week2-3706: Accepted

--- 2021/april/week2/3706/main.go
+++ 2021/april/week2/3706/main.go
@@ -4,142 +4,127 @@ import (
 	"math"
 )
 
-// This is the interface that allows for creating nested lists.
-// You should not implement it, or speculate about its implementation
-type NestedInteger struct {
-	Integer int
-	Integers []int
-	Next *NestedInteger
-}
-
-// Return true if this NestedInteger holds a single integer, rather than a nested list.
-func (this NestedInteger) IsInteger() bool {
-	return len(this.Integers) > 0
-}
-
-// Return the single integer that this NestedInteger holds, if it holds a single integer
-// The result is undefined if this NestedInteger holds a nested list
-// So before calling this method, you should have a check
-func (this NestedInteger) GetInteger() int {
-	if this.IsInteger() {
-		result := this.Integer
-		this = *this.Next
-		return result
-	}
-	result := this.Integers[0]
-	if len(this.Integers) == 1 {
-		this = *this.Next
-	} else {
-		this.Integers = this.Integers[1:]
-	}
-	return result
-}
-
-// Set this NestedInteger to hold a single integer.
-func (n *NestedInteger) SetInteger(value int) {
-	n.Integer = value
-}
-
-// Set this NestedInteger to hold a nested list and adds a nested integer to it.
-func (this *NestedInteger) Add(elem NestedInteger) {
-	if this.Next == nil {
-		this.Next = &elem
-		return
-	}
-	this.Next.Add(elem)
-}
-
-// Return the nested list that this NestedInteger holds, if it holds a nested list
-// The list length is zero if this NestedInteger holds a single integer
-// You can access NestedInteger's List element directly if you want to modify it
-func (this NestedInteger) GetList() []*NestedInteger {
-	result := []*NestedInteger{&this}
-	for this.Next != nil {
-		result = append(result, this.Next)
-		this.Next = this.Next.Next
-	}
-	return result
-}
-
 type NestedIterator struct {
-    NestedList []*NestedInteger
+    list []*NestedInteger
+    i int
+    cur *NestedIterator
 }
 
 func Constructor(nestedList []*NestedInteger) *NestedIterator {
-    return &NestedIterator{NestedList: nestedList}
+    return &NestedIterator{list: nestedList}
 }
 
 func (this *NestedIterator) Next() int {
-    if !this.HasNext() {
-        return math.MinInt32
-    }
-    head := this.NestedList[0]
-
-    if head.IsInteger() {
-        this.NestedList = this.NestedList[1:]
-        return head.GetInteger()
+    if this.list[this.i].IsInteger() {
+        res := this.list[this.i].GetInteger()
+        this.i++
+        return res
     }
 
-    list := head.GetList()
-    if len(list) > 0 {
-        current := list[0]
-        // fmt.Printf("current = %v\n", current)
-        for len(list) > 0 {
-            if current.IsInteger() {
-                break
-            } else {
-                list = current.List
-                current = list[0]
-                continue
-            }
-            current = list[0]
-            list = list[1:]
-        }
-        if len(list) > 1 {
-            list = list[1:]
-        } else {
-            list = []*NestedInteger{}
-            if len(this.NestedList) > 0 {
-                this.NestedList = this.NestedList[1:]
-            } else {
-                this.NestedList = []*NestedInteger{}
-            }
-        }
-        head.List = list
-        return current.GetInteger()
-    }
-
-    if len(this.NestedList) > 0 {
-        this.NestedList = this.NestedList[1:]
-    } else {
-        this.NestedList = []*NestedInteger{}
-    }
-    return this.Next()
+    return this.cur.Next()
 }
 
 func (this *NestedIterator) HasNext() bool {
-    // fmt.Printf("this.NestedList = %v\n", this.NestedList)
-    if len(this.NestedList) == 0 {
+    if this.i >= len(this.list) {
         return false
     }
-    head := this.NestedList[0]
+    if this.list[this.i].IsInteger() {
+        return true
+    }
+    if this.cur == nil {
+        this.cur = Constructor(this.list[this.i].GetList())
+    }
 
-    if head.IsInteger() {
+    if this.cur.HasNext() {
         return true
     }
-    list := head.GetList()
-    if len(list) > 0 {
-        // fmt.Printf("list = %v\n", list[0])
-        if len(list) == 1 && !list[0].IsInteger() && len(list[0].GetList()) == 0 {
-            return false
-        }
-        return true
-    }
-    if len(this.NestedList) == 1 {
-        return false
-    }
-    return true
+
+    this.i++
+    this.cur = nil
+
+    return this.HasNext()
 }
+
+// NG Solution (Wrong Answer)
+// type NestedIterator struct {
+//     NestedList []*NestedInteger
+// }
+// 
+// func Constructor(nestedList []*NestedInteger) *NestedIterator {
+//     return &NestedIterator{NestedList: nestedList}
+// }
+// 
+// func (this *NestedIterator) Next() int {
+//     if !this.HasNext() {
+//         return math.MinInt32
+//     }
+//     head := this.NestedList[0]
+// 
+//     if head.IsInteger() {
+//         this.NestedList = this.NestedList[1:]
+//         return head.GetInteger()
+//     }
+// 
+//     list := head.GetList()
+//     if len(list) > 0 {
+//         current := list[0]
+//         // fmt.Printf("current = %v\n", current)
+//         for len(list) > 0 {
+//             if current.IsInteger() {
+//                 break
+//             } else {
+//                 list = current.List
+//                 current = list[0]
+//                 continue
+//             }
+//             current = list[0]
+//             list = list[1:]
+//         }
+//         if len(list) > 1 {
+//             list = list[1:]
+//         } else {
+//             list = []*NestedInteger{}
+//             if len(this.NestedList) > 0 {
+//                 this.NestedList = this.NestedList[1:]
+//             } else {
+//                 this.NestedList = []*NestedInteger{}
+//             }
+//         }
+//         head.List = list
+//         return current.GetInteger()
+//     }
+// 
+//     if len(this.NestedList) > 0 {
+//         this.NestedList = this.NestedList[1:]
+//     } else {
+//         this.NestedList = []*NestedInteger{}
+//     }
+//     return this.Next()
+// }
+// 
+// func (this *NestedIterator) HasNext() bool {
+//     // fmt.Printf("this.NestedList = %v\n", this.NestedList)
+//     if len(this.NestedList) == 0 {
+//         return false
+//     }
+//     head := this.NestedList[0]
+// 
+//     if head.IsInteger() {
+//         return true
+//     }
+//     list := head.GetList()
+//     if len(list) > 0 {
+//         // fmt.Printf("list = %v\n", list[0])
+//         if len(list) == 1 && !list[0].IsInteger() && len(list[0].GetList()) == 0 {
+//             return false
+//         }
+//         return true
+//     }
+//     if len(this.NestedList) == 1 {
+//         return false
+//     }
+//     return true
+// }
 
 func main() {
 	ni1 := NestedInteger{Integers: []int{1, 1}}
