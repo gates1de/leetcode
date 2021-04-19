b5a82018b384f0b9228c4ce3da518418a18f6009
gates1de <g031i043@gmail.com>
2021-04-20 08:34:03 +0900

    2021-april-week3-3713: Accepted

--- 2021/april/week3/3713/main.go
+++ 2021/april/week3/3713/main.go
@@ -4,6 +4,20 @@ import (
 )
 
 func combinationSum4(nums []int, target int) int {
+    dp := make([]int, target + 1)
+    dp[0] = 1
+    for i := 1; i <= target; i++ {
+        for _, num := range nums {
+            if i - num >= 0 {
+                dp[i] += dp[i - num]
+            }
+        }
+    }
+    return dp[target]
+}
+
+// Time Limit Exceeded
+func ngSolution(nums []int, target int) int {
 	list := make([]int, 0)
 	for _, num := range nums {
 		current := num
