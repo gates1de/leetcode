83882cc1b4746a3fc3e7a3842ace7c6ce9dfa715
gates1de <g031i043@gmail.com>
2021-04-22 13:24:44 +0900

    2021-april-week3-3715: Accepted

--- 2021/april/week3/3715/main.go
+++ 2021/april/week3/3715/main.go
@@ -4,6 +4,26 @@ import (
 )
 
 func minimumTotal(triangle [][]int) int {
+    if len(triangle) == 0 {
+        return 0
+    }
+    if len(triangle) == 1 {
+        return triangle[0][0]
+    }
+    for i := len(triangle) - 2; i >= 0; i-- {
+        for j := 0; j < len(triangle[i]); j++ {
+            min := triangle[i + 1][j]
+            if triangle[i + 1][j + 1] < min {
+                min = triangle[i + 1][j + 1]
+            }
+            triangle[i][j] += min
+        }
+    }
+    return triangle[0][0]
+}
+
+// Out of memory
+func ngSolution(triangle [][]int) int {
 	if len(triangle) == 1 {
 		return triangle[0][0]
 	}
@@ -42,13 +62,13 @@ func min(nums []int) int {
 
 func main() {
 	// result: 11
-	// triangle := [][]int{{2}, {3, 4}, {6, 5, 7}, {4, 1, 8, 3}}
+	triangle := [][]int{{2}, {3, 4}, {6, 5, 7}, {4, 1, 8, 3}}
 
 	// result: -10
 	// triangle := [][]int{{-10}}
 
 	// result: -1
-	triangle := [][]int{{-1}, {2, 3}, {1, -1, -3}}
+	// triangle := [][]int{{-1}, {2, 3}, {1, -1, -3}}
 
 	// result: 
 	// triangle := [][]int{}
