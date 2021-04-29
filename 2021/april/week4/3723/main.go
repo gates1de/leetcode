f89f0185a3f0ab4ade793fc98f57daf6cc84b0c7
gates1de <g031i043@gmail.com>
2021-04-29 11:36:40 +0900

    2021-april-week4-3723: Accepted

--- 2021/april/week4/3723/main.go
+++ 2021/april/week4/3723/main.go
@@ -3,7 +3,44 @@ import (
 	"fmt"
 )
 
-func uniquePathsWithObstacles(obstacleGrid [][]int) int {
+func uniquePathsWithObstacles(grid [][]int) int {
+    m := len(grid)
+    n := len(grid[0])
+
+    dp := make([][]int, m)
+    for i := 0; i < m; i++ {
+        dp[i] = make([]int, n)
+    }
+
+    for i := 0; i < m; i++ {
+        for j := 0; j < n; j++ {
+            if grid[i][j] == 1 {
+                continue
+            }
+
+            if i == 0 && j == 0 {
+                dp[i][j] = 1
+                continue
+            }
+
+            top := 0
+            if i > 0 {
+                top = dp[i - 1][j]
+            }
+
+            left := 0
+            if j > 0 {
+                left = dp[i][j - 1]
+            }
+
+            dp[i][j] = top + left
+        }
+    }
+    return dp[m - 1][n - 1]
+}
+
+// Time Limit Exceeded
+func ngSolution(obstacleGrid [][]int) int {
 	if len(obstacleGrid) == 0 {
 		return 0
 	} else if len(obstacleGrid) == 1 {
@@ -53,13 +90,13 @@ func move(currentX int, currentY int, grid [][]int, goalCount *int) {
 
 func main() {
 	// result: 2
-	// obstacleGrid := [][]int{{0, 0, 0}, {0, 1, 0}, {0, 0, 0}}
+	obstacleGrid := [][]int{{0, 0, 0}, {0, 1, 0}, {0, 0, 0}}
 
 	// result: 1
 	// obstacleGrid := [][]int{{0, 1}, {0, 0}}
 
 	// result: 0
-	obstacleGrid := [][]int{{0, 0}, {0, 1}}
+	// obstacleGrid := [][]int{{0, 0}, {0, 1}}
 
 	// result: 
 	// obstacleGrid := [][]int{}
