f55105a28114feb188761761d39e597ae2c04071
gates1de <g031i043@gmail.com>
2021-05-06 10:55:29 +0900

    2021-may-week1-3732: Accepted

--- 2021/may/week1/3732/main.go
+++ 2021/may/week1/3732/main.go
@@ -4,6 +4,24 @@ import (
 )
 
 func jump(nums []int) int {
+	result := int(0)
+	curEnd := int(0)
+	curFarthest := int(0)
+
+	for i := 0; i < len(nums) - 1; i++ {
+		if curFarthest < i + nums[i] {
+			curFarthest = i + nums[i]
+		}
+		if i == curEnd {
+			result++
+			curEnd = curFarthest
+		}
+	}
+	return result
+}
+
+// Wrong Answer
+func ngSolution(nums []int) int {
 	goal := len(nums) - 1
 	result := int(0)
 	i := int(0)
