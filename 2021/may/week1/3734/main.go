436f7bd1b09b6c6cf721207ebe0ce0378e9cb041
gates1de <g031i043@gmail.com>
2021-05-08 12:37:43 +0900

    2021-may-week1-3734: Accepted

--- 2021/may/week1/3734/main.go
+++ 2021/may/week1/3734/main.go
@@ -4,6 +4,24 @@ import (
 )
 
 func minDistance(word1 string, word2 string) int {
+	dp := make([]int, len(word2))
+	for i := 0; i < len(word1); i++ {
+		var prev int
+		for j := 0; j < len(word2); j++ {
+			temp := dp[j]
+			if word1[i] == word2[j] {
+				dp[j] = prev + 1
+			} else if j > 0 && dp[j - 1] > dp[j] {
+				dp[j] = dp[j - 1]
+			}
+			prev = temp
+		}
+	}
+	return len(word1) + len(word2) - 2 * dp[len(word2) - 1]
+}
+
+// Wrong Answer
+func ngSolution(word1 string, word2 string) int {
 	shortWord := word1
 	longWord := word2
 	if len(word2) < len(word1) {
