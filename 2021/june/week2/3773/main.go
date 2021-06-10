c29470a92faa152fce1597e2a386fccee5de7b2a
gates1de <g031i043@gmail.com>
2021-06-10 14:49:21 +0900

    2021-june-week2-3773: Accepted

--- 2021/june/week2/3773/main.go
+++ 2021/june/week2/3773/main.go
@@ -5,6 +5,24 @@ import (
 )
 
 func maxResult(nums []int, k int) int {
+	var dq []int
+	dq = append(dq, 0)
+	for i := 1; i < len(nums); i++ {
+		if dq[0] + k < i {
+			dq = dq[1:]
+		}
+		nums[i] += nums[dq[0]]
+		for len(dq) > 0 && nums[dq[len(dq) - 1]] <= nums[i] {
+			dq = dq[:len(dq) - 1]
+		}
+		dq = append(dq, i)
+	}
+
+	return nums[len(nums) - 1]
+}
+
+// Time Limit Exceeded
+func ngSolution(nums []int, k int) int {
 	if len(nums) == 1 {
 		return nums[0]
 	}
