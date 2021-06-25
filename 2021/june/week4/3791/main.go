c2b3ea9968271c0dc17ddc3c2a289f42945c86cc
gates1de <g031i043@gmail.com>
2021-06-25 16:50:49 +0900

    2021-june-week4-3791: Accepted

--- 2021/june/week4/3791/main.go
+++ 2021/june/week4/3791/main.go
@@ -3,7 +3,38 @@ import (
 	"fmt"
 )
 
+// REF: Union Find Algorithm ( https://www.slideshare.net/chokudai/union-find-49066733 )
+
+func FindRoot(p int, arr []int) int {
+    for p != arr[p] {
+        p = arr[p]
+    }
+    return p
+}
+
+func Union(p, q int, arr []int) {
+    arr[q] = p
+}
+
 func findRedundantConnection(edges [][]int) []int {
+    arr := make([]int, len(edges) + 1)
+    for index := range arr {
+        arr[index] = index
+    }
+    for _, edge := range edges {
+        pRoot := FindRoot(edge[0], arr)
+        qRoot := FindRoot(edge[1], arr)
+        if pRoot == qRoot {
+            return edge
+        } else {
+            Union(pRoot, qRoot, arr)
+        }
+    }
+    return nil
+}
+
+// Wrong Answer
+func ngSolution(edges [][]int) []int {
 	result := make([]int, 2)
 	visited := map[int]bool{}
 	for _, edge := range edges {
