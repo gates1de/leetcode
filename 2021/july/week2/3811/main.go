cfc7bab14815841839f7e9c7e9901422d00f7b56
gates1de <g031i043@gmail.com>
2021-07-12 16:56:56 +0900

    2021-july-week2-3811: Accepted

---
+++ 2021/july/week2/3811/main.go
@@ -0,0 +1,68 @@
+package main
+import (
+	"fmt"
+)
+
+func isIsomorphic(s string, t string) bool {
+	// 32 is space (start character) in ascii
+	sAsciiByteNum := int(32)
+	tAsciiByteNum := int(32)
+	sMemo := map[byte]byte{}
+	tMemo := map[byte]byte{}
+	for i := 0; i < len(s); i++ {
+		sByte := s[i]
+		nextS := ""
+		if sMemo[sByte] == 0 {
+			sMemo[sByte] = byte(sAsciiByteNum)
+			nextS = string(byte(sAsciiByteNum))
+			sAsciiByteNum++
+		} else {
+			b := sMemo[sByte]
+			nextS = string(b)
+		}
+
+		tByte := t[i]
+		nextT := ""
+		if tMemo[tByte] == 0 {
+			tMemo[tByte] = byte(tAsciiByteNum)
+			nextT = string(byte(tAsciiByteNum))
+			tAsciiByteNum++
+		} else {
+			b := tMemo[tByte]
+			nextT = string(b)
+		}
+		if nextS != nextT {
+			return false
+		}
+		// fmt.Printf("sByte = %v, tByte = %v\n", sByte, tByte)
+		// fmt.Printf("newS = %v, newT = %v\n", newS, newT)
+	}
+	// fmt.Printf("sMemo = %v, tMemo = %v, newS = %v, newT = %v\n", sMemo, tMemo, newS, newT)
+	return true
+}
+
+func main() {
+	// result: true
+	// s := "egg"
+	// t := "add"
+
+	// result: false
+	// s := "foo"
+	// t := "bar"
+
+	// result: true
+	// s := "paper"
+	// t := "title"
+
+	// result: false
+	s := "egg*032@P(g@upig"
+	t := "add)032@P(g@upig"
+
+	// result: 
+	// s := ""
+	// t := ""
+
+	result := isIsomorphic(s, t)
+	fmt.Printf("result = %v\n", result)
+}
+
