8c2a24d160b42096fe557d5d86914bec402c2313
gates1de <g031i043@gmail.com>
2021-08-09 09:11:33 +0900

    2021-aug-week2-3843: Accepted

--- 2021/aug/week2/3843/main.go
+++ 2021/aug/week2/3843/main.go
@@ -2,7 +2,99 @@ package main
 import (
 	"fmt"
 	"container/heap"
+	"sort"
 )
+
+type Point struct {
+    X int
+    Y int
+}
+
+type UnionFind struct {
+	Forest map[int]int
+}
+
+func NewUnion() UnionFind {
+	return UnionFind{Forest: make(map[int]int)}
+}
+
+func (t *UnionFind) Add(x ...int) {
+    for _, e := range x {
+        t.Forest[e] = e
+    }
+}
+
+func (t *UnionFind) Find(x int) int {
+	if t.Forest[x] == x {
+        return t.Forest[x]
+	}
+	// path compressed
+    t.Forest[x] = t.Find(t.Forest[x])
+    return t.Forest[x]
+}
+
+func (t *UnionFind) Union(x, y int) {
+    t.Forest[t.Find(x)] = t.Find(y)
+}
+
+func (t *UnionFind) Group() map[int][]int {
+	ret := make(map[int][]int)
+	for k := range t.Forest {
+		group := t.Find(k)
+		ret[group] = append(ret[group], k)
+	}
+	return ret
+}
+
+func matrixRankTransform(matrix [][]int) [][]int {
+	n, m := len(matrix), len(matrix[0])
+	rank := make([]int, m + n)
+
+	// prepare matrix coordinate map
+	coordMap := make(map[int][]Point)
+	for y := range matrix {
+		for x := range matrix[y] {
+            coordMap[matrix[y][x]] = append(coordMap[matrix[y][x]], Point{X: x, Y: y})
+        }
+	}
+
+	// sort map keys
+	coordKeys := make([]int, 0, len(coordMap))
+	for k := range coordMap {
+		coordKeys = append(coordKeys, k)
+	}
+	sort.Ints(coordKeys)
+
+	// iterate coordinate map keys
+	for _, key := range coordKeys {
+		dsu := NewUnion()
+
+		for _, p := range coordMap[key] {
+            dsu.Add(p.Y, p.X + n)
+		}
+
+		for _, p := range coordMap[key] {
+			dsu.Union(p.Y, p.X + n)
+		}
+
+		for _, group := range dsu.Group() {
+            max := rank[group[0]]
+            for _, g := range group {
+                if rank[g] > max {
+                    max = rank[g]
+                }
+            }
+			for _, g := range group {
+				rank[g] = max + 1
+			}
+		}
+
+		for _, p := range coordMap[key] {
+			matrix[p.Y][p.X] = rank[p.Y]
+        }
+	}
+	return matrix
+}
 
 type Cell struct {
     Val int
@@ -21,7 +113,8 @@ func (h *RankHeap) Pop() (v interface{}) {
 }
 func (h *RankHeap) Push(v interface{}) { *h = append(*h, v.(Cell)) }
 
-func matrixRankTransform(matrix [][]int) [][]int {
+// Wrong Answer
+func ngSolution(matrix [][]int) [][]int {
 	answer := make([][]int, len(matrix))
 	rankHeap := &RankHeap{}
 	heap.Init(rankHeap)
@@ -125,15 +218,27 @@ func main() {
 	// }
 
 	// result: [[10,3,4,9,5,15,8],[12,4,2,10,1,13,14],[11,13,9,8,6,7,12],[2,10,15,4,9,3,15],[1,2,17,16,7,15,3],[5,14,18,11,10,8,4],[3,15,5,6,8,14,7]]
-	matrix := [][]int{
-		{-2,-35,-32,-5,-30,33,-12},
-		{7,2,-43,4,-49,14,17},
-		{4,23,-6,-15,-24,-17,6},
-		{-47,20,39,-26,9,-44,39},
-		{-50,-47,44,43,-22,33,-36},
-		{-13,34,49,24,23,-2,-35},
-		{-40,43,-22,-19,-4,23,-18},
-	}
+	// matrix := [][]int{
+	// 	{-2,-35,-32,-5,-30,33,-12},
+	// 	{7,2,-43,4,-49,14,17},
+	// 	{4,23,-6,-15,-24,-17,6},
+	// 	{-47,20,39,-26,9,-44,39},
+	// 	{-50,-47,44,43,-22,33,-36},
+	// 	{-13,34,49,24,23,-2,-35},
+	// 	{-40,43,-22,-19,-4,23,-18},
+	// }
+
+	// result: [[7,13,1,5,4,6,9,8],[8,11,2,10,1,12,14,9],[2,14,1,11,13,7,5,3],[3,19,16,12,14,7,10,13],[8,12,6,14,5,1,4,13],[2,16,15,17,4,18,3,14],[3,7,11,6,12,13,14,10],[16,19,18,3,15,2,11,17]]
+    matrix := [][]int{
+        {-23,20,-49,-30,-39,-28,-5,-14},
+        {-19,4,-33,2,-47,28,43,-6},
+        {-47,36,-49,6,17,-8,-21,-30},
+        {-27,44,27,10,21,-8,3,14},
+        {-19,12,-25,34,-27,-48,-37,14},
+        {-47,40,23,46,-39,48,-41,18},
+        {-27,-4,7,-10,9,36,43,2},
+        {37,44,43,-38,29,-44,19,38},
+    }
 
 	// result: 
 	// matrix := [][]int{}
