d7e81eb11172a29896894f9ecfc1beabedfa0139
gates1de <g031i043@gmail.com>
2021-08-14 17:53:13 +0900

    2021-aug-week2-3849: Accepted

--- 2021/aug/week2/3849/main.go
+++ 2021/aug/week2/3849/main.go
@@ -3,7 +3,36 @@ import (
 	"fmt"
 )
 
+// REF: https://leetcode-cn.com/problems/remove-boxes/solution/yi-chu-he-zi-by-leetcode-solution/
 func removeBoxes(boxes []int) int {
+    dp := [100][100][100]int{}
+    var calculatePoints func(boxes []int, l, r, k int) int
+    calculatePoints = func(boxes []int, l, r, k int) int {
+        if l > r {
+            return 0
+        }
+        if dp[l][r][k] == 0 {
+            dp[l][r][k] = calculatePoints(boxes, l, r - 1, 0) + (k + 1) * (k + 1)
+            for i := l; i < r; i++ {
+                if boxes[i] == boxes[r] {
+                    dp[l][r][k] = max(dp[l][r][k], calculatePoints(boxes, l, i, k + 1) + calculatePoints(boxes, i + 1, r - 1, 0))
+                }
+            }
+        }
+        return dp[l][r][k]
+    }
+    return calculatePoints(boxes, 0, len(boxes) - 1, 0)
+}
+
+func max(a, b int) int {
+    if a > b {
+        return a
+    }
+    return b
+}
+
+// Time Limit Exceeded
+func ngSolution(boxes []int) int {
 	if len(boxes) == 1 {
 		return 1
 	}
