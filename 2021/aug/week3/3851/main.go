a103ef0629f870b72d9fdfac9826d0c46f0a4b89
gates1de <g031i043@gmail.com>
2021-08-15 18:14:35 +0900

    2021-aug-week3-3851: Accepted

--- 2021/aug/week3/3851/main.go
+++ 2021/aug/week3/3851/main.go
@@ -1,9 +1,44 @@
 package main
 import (
 	"fmt"
+	"math"
 )
 
 func minWindow(s string, t string) string {
+    if len(s) < len(t) { return "" }
+    m := make(map[byte]int)
+    for _, v := range []byte(t) {
+        m[v]++
+    }
+    start, end, index := 0, 0, 0
+    counter := len(t)
+    minLen := math.MaxInt32
+    for end < len(s) {
+        if m[s[end]] > 0 {
+            counter--
+        }
+        m[s[end]]--
+        end++
+        for counter == 0 {
+            if minLen > end - start {
+                minLen = end - start
+                index = start
+            }
+            m[s[start]]++
+            if m[s[start]] == 1 {
+                counter++
+            }
+            start++
+        }
+    }
+    if minLen == math.MaxInt32 {
+        return ""
+    }
+    return s[index:index+minLen]
+}
+
+// Time Limit Exceeded
+func ngSolution(s string, t string) string {
 	if len(s) < len(t) {
 		return ""
 	} else if len(s) == len(t) {
