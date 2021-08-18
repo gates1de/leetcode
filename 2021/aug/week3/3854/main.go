b25a728781d91c980d22f6a6ff5c958764e4c62b
gates1de <g031i043@gmail.com>
2021-08-18 19:20:04 +0900

    2021-aug-week3-3854: Passed cases 23 / 269

---
+++ 2021/aug/week3/3854/main.go
@@ -0,0 +1,63 @@
+package main
+import (
+	"fmt"
+	"strconv"
+)
+
+func numDecodings(s string) int {
+	if len(s) == 1 {
+		if s == "0" {
+			return 0
+		}
+		return 1
+	}
+
+	result := int(0)
+	helper(1, s, &result)
+	helper(2, s, &result)
+	return result
+}
+
+func helper(digit int, s string, result *int) {
+	// fmt.Printf("digit = %v, s = %v\n", digit, s)
+	if len(s) == 0 {
+		*result++
+		return
+	}
+
+	char := s[:digit]
+	// fmt.Printf("char = %v\n", char)
+	num, _ := strconv.Atoi(char)
+	if char[0] == byte('0') || (num <= 0 || 27 <= num) {
+		return
+	}
+
+	helper(1, s[digit:], result)
+	if len(s) >= 2 && len(s[digit:]) >= 2 {
+		helper(2, s[digit:], result)
+	}
+}
+
+func main() {
+	// result: 2
+	// s := "12"
+
+	// result: 3
+	// s := "226"
+
+	// result: 0
+	// s := "0"
+
+	// result: 0
+	// s := "06"
+
+	// result: 
+	s := "111111111111111111111111111111111111111111111"
+
+	// result: 
+	// s := ""
+
+	result := numDecodings(s)
+	fmt.Printf("result = %v\n", result)
+}
+
