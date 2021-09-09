507a3d2f711889b546c4508329acc3cdd7598248
gates1de <g031i043@gmail.com>
2021-09-09 18:00:45 +0900

    2021-sep-week2-3880: Accepted

--- 2021/sep/week2/3880/main.go
+++ 2021/sep/week2/3880/main.go
@@ -4,6 +4,61 @@ import (
 )
 
 func orderOfLargestPlusSign(n int, mines [][]int) int {
+    grid := make([][]int, n)
+    for i := range grid {
+        grid[i] = make([]int, n)
+    }
+    for i := 0; i < n; i++ {
+        for j := 0; j < n; j++ {
+            grid[i][j] = n
+        }
+    }
+    for _, v := range mines {
+        grid[v[0]][v[1]] = 0
+    }
+    for i := 0; i < n; i++ {
+        left := 0
+        right := 0
+        up := 0
+        down := 0
+        for j, k := 0, n - 1; j < n; j, k = j + 1, k - 1 {
+            if grid[i][j] == 0 {
+                left = 0
+            } else {
+                left += 1
+            }
+            if left < grid[i][j] { grid[i][j] = left }
+            if grid[i][k] == 0 {
+                right = 0
+            } else {
+                right += 1
+            }
+            if right < grid[i][k] { grid[i][k] = right }
+            if grid[j][i] == 0 {
+                up = 0
+            } else {
+                up += 1
+            }
+            if up < grid[j][i] { grid[j][i] = up }
+            if grid[k][i] == 0 {
+                down = 0
+            } else {
+                down += 1
+            }
+            if down < grid[k][i] { grid[k][i] = down }
+        }
+    }
+    res := 0
+    for i := range grid {
+        for j := range grid[0] {
+            if grid[i][j] > res { res = grid[i][j] }
+        }
+    }
+    return res
+}
+
+// Time Limit Exceeded
+func ngSolution(n int, mines [][]int) int {
 	if n * n <= len(mines) {
 		return 0
 	}
