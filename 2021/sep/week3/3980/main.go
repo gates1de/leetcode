d0edc37bcabb0d86c114099d506c42c5ea57445d
gates1de <g031i043@gmail.com>
2021-09-19 23:59:59 +0900

    2021-sep-week3-3980: Accepted

--- 2021/sep/week3/3980/main.go
+++ 2021/sep/week3/3980/main.go
@@ -4,6 +4,29 @@ import (
 )
 
 func numDistinct(s string, t string) int {
+	m := make([][]int, len(t) + 1)
+	for i := range m {
+		m[i] = make([]int, len(s) + 1)
+	}
+
+	for i := 0; i <= len(s); i++ {
+		m[0][i] = 1
+	}
+
+	for i := 0; i < len(t); i++ {
+		for j := 0; j < len(s); j++ {
+			if t[i] == s[j] {
+				m[i + 1][j + 1] = m[i][j] + m[i + 1][j]
+			} else {
+				m[i + 1][j + 1] = m[i + 1][j]
+			}
+		}
+	}
+	return m[len(t)][len(s)]
+}
+
+// Wrong Answer
+func ngSolution(s string, t string) int {
 	result := int(0)
 	newS := ""
 	m := map[rune]bool{}
