6d086003bdd5f7b0a284de22caccce57dca85ad9
gates1de <g031i043@gmail.com>
2021-10-04 10:21:16 +0900

    2021-oct-week2-463: Accepted

---
+++ 2021/oct/week2/463/main.go
@@ -0,0 +1,70 @@
+package main
+import (
+	"fmt"
+)
+
+func islandPerimeter(grid [][]int) int {
+	ver := map[[2]int]bool{}
+	hor := map[[2]int]bool{}
+	result := int(0)
+	for i, row := range grid {
+		for j, cell := range row {
+			if cell == 0 {
+				continue
+			}
+
+			if !ver[[2]int{i, j}] {
+				result++
+				ver[[2]int{i, j}] = true
+			} else {
+				result--
+			}
+			if !ver[[2]int{i + 1, j}] {
+				result++
+				ver[[2]int{i + 1, j}] = true
+			} else {
+				result--
+			}
+
+			if !hor[[2]int{i, j}] {
+				result++
+				hor[[2]int{i, j}] = true
+			} else {
+				result--
+			}
+			if !hor[[2]int{i, j + 1}] {
+				result++
+				hor[[2]int{i, j + 1}] = true
+			} else {
+				result--
+			}
+			// fmt.Printf("[%v, %v]: result = %v\n", i, j, result)
+		}
+	}
+
+	return result
+}
+
+func main() {
+	// result: 16
+	// grid := [][]int{
+	// 	{0, 1, 0, 0},
+	// 	{1, 1, 1, 0},
+	// 	{0, 1, 0, 0},
+	// 	{1, 1, 0, 0},
+	// }
+
+	// result: 4
+	// grid := [][]int{{1}}
+
+	// result: 4
+	grid := [][]int{{1,0}}
+
+	// result: 
+	// grid := [][]int{
+	// }
+
+	result := islandPerimeter(grid)
+	fmt.Printf("result = %v\n", result)
+}
+
