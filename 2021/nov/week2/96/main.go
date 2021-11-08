40a8bae4f674bc74fa459e30a0dafc5bd10bcbe9
gates1de <g031i043@gmail.com>
2021-11-08 12:49:55 +0900

    2021-nov-week2-96: Accepted

--- 2021/nov/week2/96/main.go
+++ 2021/nov/week2/96/main.go
@@ -4,6 +4,19 @@ import (
 )
 
 func numTrees(n int) int {
+	dp := make([]int, n + 1)
+	dp[0], dp[1] = 1, 1
+
+	for i := 2; i <= n; i++ {
+		for j := 1; j <= i; j++ {
+			dp[i] += dp[j - 1] * dp[i - j]
+		}
+	}
+	return dp[n]
+}
+
+// Wrong Answer
+func ngSolution(n int) int {
 	result := int(0)
 	for i := 1; i <= n; i++ {
 		m := map[int]bool{}
