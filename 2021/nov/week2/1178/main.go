690657f607269c8c9050822d569d9c93cabb72cf
gates1de <g031i043@gmail.com>
2021-11-09 15:39:50 +0900

    2021-nov-week2-1178: Accepted

--- 2021/nov/week2/1178/main.go
+++ 2021/nov/week2/1178/main.go
@@ -4,6 +4,47 @@ import (
 )
 
 func findNumOfValidWords(words []string, puzzles []string) []int {
+    freqWords := make(map[int]int)
+    for _, word := range words {
+        b := bits(word)
+        if _, ok := freqWords[b]; !ok {
+            freqWords[b] = 1
+        } else {
+            freqWords[b] += 1
+        }
+    }
+
+    result := make([]int, len(puzzles))
+    for i, puzzle := range puzzles {
+        mask := bits(puzzle)
+        num := 0
+
+        fb := 1 << (puzzle[0] - 'a')
+
+        for cur := mask; cur != 0; cur = (cur-1) & mask {
+            if cur & fb == 0 {
+                continue
+            }
+            if v, ok := freqWords[cur]; ok {
+                num += v
+            }
+        }
+        result[i] = num
+    }
+    return result
+}
+
+
+func bits(s string) int {
+    var bits int
+    for _, l := range s {
+        bits |= 1 << (l - 'a')
+    }
+    return bits
+}
+
+// Time Limit Exceeded
+func ngSolution(words []string, puzzles []string) []int {
 	result := make([]int, len(puzzles))
 
 	m := make([]map[rune]int, len(words))
@@ -41,12 +82,12 @@ func isOK(puzzle string, word string, m map[rune]int) bool {
 
 func main() {
 	// result: [1,1,3,2,4,0]
-	// words := []string{"aaaa","asas","able","ability","actt","actor","access"}
-	// puzzles := []string{"aboveyz","abrodyz","abslute","absoryz","actresz","gaswxyz"}
+	words := []string{"aaaa","asas","able","ability","actt","actor","access"}
+	puzzles := []string{"aboveyz","abrodyz","abslute","absoryz","actresz","gaswxyz"}
 
 	// result: [0,1,3,2,0]
-	words := []string{"apple","pleas","please"}
-	puzzles := []string{"aelwxyz","aelpxyz","aelpsxy","saelpxy","xaelpsy"}
+	// words := []string{"apple","pleas","please"}
+	// puzzles := []string{"aelwxyz","aelpxyz","aelpsxy","saelpxy","xaelpsy"}
 
 	// result: 
 	// words := []string{}
