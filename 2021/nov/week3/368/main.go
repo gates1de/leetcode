65b3f2dbbf7ab01762b35c64e445581c191a6128
gates1de <g031i043@gmail.com>
2021-11-15 12:39:45 +0900

    2021-nov-week3-368: Accepted

--- 2021/nov/week3/368/main.go
+++ 2021/nov/week3/368/main.go
@@ -2,11 +2,43 @@ package main
 import (
 	"fmt"
 	"math"
+	"sort"
 )
 
-const maxNum = 2 * 1000000000
-
 func largestDivisibleSubset(nums []int) []int {
+    sort.Ints(nums)
+    max := int(1)
+    l := len(nums)
+    dp := make([]int, len(nums))
+    for i := range dp {
+		dp[i] = 1
+	}
+    for i := 1; i < l; i++ {
+        for j := 0; j < i; j++ {
+            if nums[i] % nums[j] == 0 && dp[j] + 1 > dp[i] {
+                dp[i] = dp[j] + 1
+                if dp[i] > max {
+					max = dp[i]
+				}
+            }
+        }
+    }
+
+    result := []int{}
+    prev := int(-1)
+    for i := l - 1; i >= 0; i-- {
+        if dp[i] == max && (prev == -1 || prev % nums[i] == 0) {
+            result = append(result, nums[i])
+            prev = nums[i]
+            max--
+        }
+    }
+    return result
+}
+
+// Wrong Answer
+const maxNum = 2 * 1000000000
+func ngSolution(nums []int) []int {
 	m := map[int][]int{}
 	isExistsOne := false
 	for _, num := range nums {
