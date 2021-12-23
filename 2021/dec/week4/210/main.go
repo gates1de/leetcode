eaf93a6fd1a04574ac857f20bedd202073f083c3
gates1de <g031i043@gmail.com>
2021-12-23 12:57:40 +0900

    2021-dec-week4-210: Accepted

--- 2021/dec/week4/210/main.go
+++ 2021/dec/week4/210/main.go
@@ -4,6 +4,43 @@ import (
 )
 
 func findOrder(numCourses int, prerequisites [][]int) []int {
+    edges := make([][]int, numCourses)
+    indegree := make([]int, numCourses)
+
+    for _, v := range prerequisites {
+        edges[v[1]] = append(edges[v[1]], v[0])
+        indegree[v[0]]++
+    }
+
+    queue, res := []int{}, []int{}
+    for i, v := range indegree {
+		if v == 0 {
+			queue = append(queue, i)
+		}
+	}
+
+    for len(queue) > 0 {
+        cur := queue[0]
+        queue = queue[1:]
+        numCourses--
+        res = append(res, cur)
+        for _, v := range edges[cur] {
+            indegree[v]--
+            if indegree[v] == 0 {
+				queue = append(queue, v)
+			}
+        }
+    }
+
+    if numCourses != 0 {
+		return []int{}
+	}
+
+    return res
+}
+
+// Time Limit Exceeded
+func ngSolution(numCourses int, prerequisites [][]int) []int {
 	if len(prerequisites) == 0 {
 		result := make([]int, numCourses)
 		for i := 0; i < numCourses; i++ {
