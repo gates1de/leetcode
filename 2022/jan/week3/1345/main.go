ecfbb2a45b22dd4fac1b88d4725acbee6a06d90d
gates1de <g031i043@gmail.com>
2022-01-15 10:41:59 +0900

    2022-jan-week3-1345: Accepted

--- 2022/jan/week3/1345/main.go
+++ 2022/jan/week3/1345/main.go
@@ -4,6 +4,56 @@ import (
 )
 
 func minJumps(arr []int) int {
+	m := map[int][]int{}
+	for i, num := range arr {
+		if m[num] == nil {
+			m[num] = []int{}
+		}
+		m[num] = append(m[num], i)
+	}
+
+	numVisited := map[int]bool{}
+	queue := []int{0}
+	step := int(0)
+	visited := make([]bool, len(arr))
+	visited[0] = true
+	for len(queue) > 0 {
+		size := len(queue)
+		for i := 0; i < size; i++ {
+			cursor := queue[i]
+			if cursor == len(arr) - 1 {
+				return step
+			}
+
+			num := arr[cursor]
+			if !numVisited[num] {
+				numVisited[num] = true
+				for _, j := range m[num] {
+					if j == cursor {
+						continue
+					}
+					queue = append(queue, j)
+					visited[j] = true
+				}
+			}
+
+			if cursor - 1 >= 0 && !visited[cursor - 1] {
+				queue = append(queue, cursor - 1)
+				visited[cursor - 1] = true
+			}
+			if cursor + 1 < len(arr) && !visited[cursor + 1] {
+				queue = append(queue, cursor + 1)
+				visited[cursor + 1] = true
+			}
+		}
+		queue = queue[size:]
+		step++
+	}
+	return -1
+}
+
+// Time Limit Exceeded
+func ngSolution(arr []int) int {
 	if len(arr) <= 2 {
 		return len(arr) - 1
 	}
