6b918c9df3f732d14b83cfae173270c5d13ee36e
gates1de <g031i043@gmail.com>
2022-01-29 10:55:56 +0900

    2022-jan-week5-84: Accepted

--- 2022/jan/week5/84/main.go
+++ 2022/jan/week5/84/main.go
@@ -4,6 +4,45 @@ import (
 )
 
 func largestRectangleArea(heights []int) int {
+    l := len(heights)
+    if l == 0 {
+        return 0
+    }
+    dpLeft := make([]int, l)
+    dpRight := make([]int, l)
+
+    for i := 1 ; i < l; i++ {
+        dpLeft[i] = i
+        j := i
+        for dpLeft[j] - 1 >= 0 && heights[dpLeft[j] - 1] >= heights[i] {
+            j = dpLeft[j] - 1
+        }
+        dpLeft[i] = dpLeft[j]
+    }
+
+    dpRight[l - 1] = l - 1
+    for i := l - 2; i >= 0; i-- {
+        dpRight[i] = i
+        j := i
+        for dpRight[j] + 1 <= l - 1 && heights[dpRight[j] + 1] >= heights[i] {
+            j = dpRight[j] + 1
+        }
+        dpRight[i] = dpRight[j]
+    }
+
+    result := int(0)
+
+    for i := 0; i < l; i++  {
+        if result < heights[i] * (dpRight[i] - dpLeft[i] + 1) {
+            result = heights[i] * (dpRight[i] - dpLeft[i] + 1)
+        }
+    }
+
+    return result
+}
+
+// Time Limit Exceeded
+func ngSolution(heights []int) int {
 	dp := map[int]int{}
 	helper(0, heights[0], dp, heights)
 	fmt.Println(dp)
