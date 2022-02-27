02ce7721bc1fb5325718e9ff380079db2e0882f6
gates1de <g031i043@gmail.com>
2022-02-27 19:27:26 +0900

    2022-feb-week5-662: Accepted

--- 2022/feb/week5/662/main.go
+++ 2022/feb/week5/662/main.go
@@ -11,6 +11,45 @@ type TreeNode struct {
 }
 
 func widthOfBinaryTree(root *TreeNode) int {
+	result := int(0)
+	bfs([]*TreeNode{root}, 0, []int{0}, &result)
+	return result
+}
+
+func bfs(nodes []*TreeNode, level int, order []int, width *int) {
+	if len(nodes) == 0 {
+		return
+	}
+
+	*width = max(*width, order[len(nodes) - 1] - order[0] + 1)
+	nextLevel := []*TreeNode{}
+	nextOrder := []int{}
+	for i, v := range nodes {
+		if v.Left != nil {
+			nextLevel = append(nextLevel, v.Left)
+			nextOrder = append(nextOrder, order[i] * 2)
+		}
+		if v.Right != nil {
+			nextLevel = append(nextLevel, v.Right)
+			nextOrder = append(nextOrder, order[i] * 2 + 1)
+		}
+	}
+
+	bfs(nextLevel, level + 1, nextOrder, width)
+}
+
+func max(values ...int) int {
+	maxValue := math.MinInt32
+	for _, v := range values {
+		if v > maxValue {
+			maxValue = v
+		}
+	}
+	return maxValue
+}
+
+// Wrong Answer
+func ngSolution(root *TreeNode) int {
 	levels := map[int][]int{}
 	helper(1, 1, root, levels)
 	// fmt.Println(levels)
