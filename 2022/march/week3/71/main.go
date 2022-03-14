5d2cd4fd21a3a3894766679e1bfac374ad5d2e00
gates1de <g031i043@gmail.com>
2022-03-14 10:02:41 +0900

    2022-march-week3-71: Accepted

---
+++ 2022/march/week3/71/main.go
@@ -0,0 +1,48 @@
+package main
+import (
+	"fmt"
+	"strings"
+)
+
+func simplifyPath(path string) string {
+	pathQueue := []string{}
+	for _, str := range strings.Split(path, "/") {
+		if str == "" || str == "." {
+			continue
+		} else if str == ".." {
+			if len(pathQueue) > 0 {
+				pathQueue = pathQueue[:len(pathQueue) - 1]
+			}
+			continue
+		}
+		pathQueue = append(pathQueue, str)
+		// fmt.Printf("pathQueue = %v\n", pathQueue)
+	}
+
+	result := "/"
+	slash := ""
+	for _, str := range pathQueue {
+		result += slash + str
+		slash = "/"
+	}
+
+	return result
+}
+
+func main() {
+	// result: "/home"
+	// path := "/home/"
+
+	// result: "/"
+	// path := "/../"
+
+	// result: "/home/foo"
+    // path := "/home//foo/"
+
+	// result: "/c"
+	path := "/a/./b/../../c/"
+
+	result := simplifyPath(path)
+	fmt.Printf("result = %v\n", result)
+}
+
