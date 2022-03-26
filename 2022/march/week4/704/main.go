80a9f70a524ae700cedf6b18fa7f587f5520ab73
gates1de <g031i043@gmail.com>
2022-03-26 09:57:23 +0900

    2022-march-week4-704: Accepted

---
+++ 2022/march/week4/704/main.go
@@ -0,0 +1,55 @@
+package main
+import (
+	"fmt"
+)
+
+func search(nums []int, target int) int {
+	return binarySearch(0, nums, target)
+}
+
+func binarySearch(index int, nums []int, target int) int {
+	if index >= len(nums) {
+		return -1
+	}
+
+	if nums[index] == target {
+		return index
+	}
+
+	left := binarySearch(2 * index + 1, nums, target)
+	if left >= 0 {
+		return left
+	}
+
+	return binarySearch(2 * index + 2, nums, target)
+}
+
+func main() {
+	// result: 4
+	// nums := []int{-1, 0, 3, 5, 9, 12}
+	// target := int(9)
+
+	// result: -1
+	// nums := []int{-1, 0, 3, 5, 9, 12}
+	// target := int(2)
+
+	// result: 9
+	// nums := []int{-32,-29,-10,-1,4,6,24,60,62,76,434,8793}
+	// target := int(76)
+
+	// result: 0
+	// nums := []int{1}
+	// target := int(1)
+
+	// result: -1
+	nums := []int{1}
+	target := int(0)
+
+	// result: 
+	// nums := []int{}
+	// target := int(0)
+
+	result := search(nums, target)
+	fmt.Printf("result = %v\n", result)
+}
+
