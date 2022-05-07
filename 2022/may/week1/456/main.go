048dabc6dbde3c6fcdd3c261382fa4169bda91d0
gates1de <g031i043@gmail.com>
2022-05-07 11:23:39 +0900

    2022-may-week1-456: Passed cases 101 / 102

---
+++ 2022/may/week1/456/main.go
@@ -0,0 +1,79 @@
+package main
+import (
+	"fmt"
+	"sort"
+)
+
+func find132pattern(nums []int) bool {
+	n := len(nums)
+	if n <= 2 {
+		return false
+	}
+
+	m := map[int]int{}
+	min := nums[0]
+	for i := 1; i < n; i++ {
+		num := nums[i]
+		if num < min {
+			min = num
+		} else {
+			m[i] = min
+		}
+	}
+
+	min = nums[n - 1]
+	max := min
+	for i := n - 2; i >= 0; i-- {
+		num := nums[i]
+		if num < min {
+			min = num
+		} else {
+			if val, ok := m[i]; ok && val < num {
+				for j := i + 1; j < n; j++ {
+					if val < nums[j] && num > nums[j] {
+						return true
+					}
+				}
+			}
+
+			if num > max {
+				max = num
+			}
+		}
+	}
+
+	return false
+}
+
+func main() {
+	// result: false
+	// nums := []int{1, 2, 3, 4}
+
+	// result: true
+	// nums := []int{3, 1, 4, 2}
+
+	// result: true
+	// nums := []int{-1, 3, 2, 0}
+
+	// result: false
+	// nums := []int{1, 0, 1, -4, -3}
+
+	// result: true
+	// nums := []int{3, 5, 0, 3, 4}
+
+	// result: false
+	// nums := []int{-2, 1, 1}
+
+	// result: true
+	// nums := []int{1,3,2,4,5,6,7,8,9,10}
+
+	// result: true
+	nums := []int{-2, 1, 2, -2, 1, 2}
+
+	// result: 
+	// nums := []int{}
+
+	result := find132pattern(nums)
+	fmt.Printf("result = %v\n", result)
+}
+
