1bc4b3c00f95ae5fc50bf19397c5eade5a8e7239
gates1de <g031i043@gmail.com>
2022-05-07 11:30:15 +0900

    2022-may-week1-456: Accepted

--- 2022/may/week1/456/main.go
+++ 2022/may/week1/456/main.go
@@ -1,10 +1,27 @@
 package main
 import (
 	"fmt"
-	"sort"
+	"math"
 )
 
 func find132pattern(nums []int) bool {
+	max := math.MinInt32
+	stack := make([]int, 0)
+	for i := len(nums) - 1; i >= 0; i-- {
+		if nums[i] < max {
+			return true
+		}
+		for len(stack) > 0 && stack[len(stack) - 1] < nums[i] {
+			max = stack[len(stack) - 1]
+			stack = stack[:len(stack) - 1]
+		}
+		stack = append(stack, nums[i])
+	}
+	return false
+}
+
+// Time Limit Exceeded
+func ngSolution(nums []int) bool {
 	n := len(nums)
 	if n <= 2 {
 		return false
