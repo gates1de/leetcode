564b94b75b682bf7e6196c28220e31760f2f171d
gates1de <g031i043@gmail.com>
2022-09-23 11:35:05 +0900

    2022-sep-week4-1680: Accepted

---
+++ 2022/sep/week4/1680/main.go
@@ -0,0 +1,36 @@
+package main
+import (
+	"fmt"
+)
+
+const modulo = int(1e9 + 7)
+
+func concatenatedBinary(n int) int {
+    result := int(1)
+    length := int(4)
+    for i := 2; i <= n; i++ {
+        if i == length {
+            length *= 2
+        }
+        result = (result * length + i) % modulo
+    }
+    return result
+}
+
+func main() {
+	// result: 1
+	// n := int(1)
+
+	// result: 27
+	// n := int(3)
+
+	// result: 505379714
+	n := int(12)
+
+	// result: 
+	// n := int(0)
+
+	result := concatenatedBinary(n)
+	fmt.Printf("result = %v\n", result)
+}
+
