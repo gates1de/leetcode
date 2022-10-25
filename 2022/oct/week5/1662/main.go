06512bb8361df17f49c5f23e5a49cd097d38c6d0
gates1de <g031i043@gmail.com>
2022-10-25 10:29:27 +0900

    2022-oct-week5-1662: Accepted

---
+++ 2022/oct/week5/1662/main.go
@@ -0,0 +1,53 @@
+package main
+import (
+	"fmt"
+)
+
+func arrayStringsAreEqual(word1 []string, word2 []string) bool {
+	str1 := ""
+	str2 := ""
+
+	n := len(word1)
+	m := len(word2)
+	limit := n
+	if n < m {
+		limit = m
+	}
+
+	i := int(0)
+	j := int(0)
+	for i < limit && j < limit {
+		if i < n {
+			str1 += word1[i]
+			i++
+		}
+		if j < m {
+			str2 += word2[j]
+			j++
+		}
+	}
+
+	return str1 == str2
+}
+
+func main() {
+	// result: true
+	// word1 := []string{"ab", "c"}
+	// word2 := []string{"a", "bc"}
+
+	// result: false
+	// word1 := []string{"a", "cb"}
+	// word2 := []string{"ab", "c"}
+
+	// result: true
+	word1 := []string{"abc", "d", "defg"}
+	word2 := []string{"abcddefg"}
+
+	// result: 
+	// word1 := []string{}
+	// word2 := []string{}
+
+	result := arrayStringsAreEqual(word1, word2)
+	fmt.Printf("result = %v\n", result)
+}
+
