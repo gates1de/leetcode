65e3283adc3b5191bf39d6a6fcc0b372a0197939
gates1de <g031i043@gmail.com>
2022-10-28 09:30:27 +0900

    2022-oct-week5-49: Accepted

---
+++ 2022/oct/week5/49/main.go
@@ -0,0 +1,44 @@
+package main
+import (
+	"fmt"
+	"sort"
+)
+
+func groupAnagrams(strs []string) [][]string {
+	m := map[string][]string{}
+	for _, str := range strs {
+		chars := []byte(str)
+		sort.Slice(chars, func(a, b int) bool { return chars[a] < chars[b] })
+		sortedStr := string(chars)
+		if _, ok := m[sortedStr]; ok {
+			m[sortedStr] = append(m[sortedStr], str)
+		} else {
+			m[sortedStr] = []string{str}
+		}
+	}
+
+	result := make([][]string, 0, len(m))
+	for _, val := range m {
+		result = append(result, val)
+	}
+
+	return result
+}
+
+func main() {
+	// result: [["bat"],["nat","tan"],["ate","eat","tea"]]
+	// strs := []string{"eat","tea","tan","ate","nat","bat"}
+
+	// result: [[""]]
+	// strs := []string{""}
+
+	// result: [["a"]]
+	strs := []string{"a"}
+
+	// result: 
+	// strs := []string{}
+
+	result := groupAnagrams(strs)
+	fmt.Printf("result = %v\n", result)
+}
+
