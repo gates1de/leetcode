060ae8379df59745dd4675e1ab9a2e7a76e71a63
gates1de <g031i043@gmail.com>
2023-08-10 13:18:18 +0900

    2023-aug-week2-81: Accepted

---
+++ 2023/aug/week1/81/main.go
@@ -0,0 +1,53 @@
+package main
+import (
+	"fmt"
+)
+
+func search(nums []int, target int) bool {
+    if len(nums) == 0 {
+        return false
+    }
+
+    if nums[0] > target{
+        return helper(-1, 0, len(nums), nums, target)
+    }
+    return helper(0, 0, len(nums), nums, target)
+}
+
+func helper(index int, count int, n int, nums []int, target int) bool {
+    if count == n {
+        return false
+    }
+
+    if index < 0 {
+        num := nums[index + n]
+        if num == target {
+            return true
+        }
+        return helper(index - 1, count + 1, n, nums, target)
+    }
+
+    num := nums[index]
+    if num == target {
+        return true
+    }
+    return helper(index + 1, count + 1, n, nums, target)
+}
+
+func main() {
+	// result: true
+	// nums := []int{2,5,6,0,0,1,2}
+	// target := int(0)
+
+	// result: false
+	nums := []int{2,5,6,0,0,1,2}
+	target := int(3)
+
+	// result: 
+	// nums := []int{}
+	// target := int(0)
+
+	result := search(nums, target)
+	fmt.Printf("result = %v\n", result)
+}
+
