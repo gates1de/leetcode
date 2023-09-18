1ee1e18299ca223f938a3b8771eff6f58bc93a9f
gates1de <g031i043@gmail.com>
2023-09-18 21:13:27 +0900

    2023-sep-week4-1337: Accepted

---
+++ 2023/sep/week4/1337/main.go
@@ -0,0 +1,70 @@
+package main
+import (
+	"fmt"
+	"sort"
+)
+
+func kWeakestRows(mat [][]int, k int) []int {
+    counts := make([][2]int, len(mat))
+    for i, nums := range mat {
+        counts[i][0] = i
+        counts[i][1] = oneCounter(nums)
+    }
+
+    sort.Slice(counts, func(a, b int) bool {
+        if counts[a][1] == counts[b][1] {
+            return counts[a][0] < counts[b][0]
+        }
+        return counts[a][1] < counts[b][1]
+    })
+
+    result := make([]int, k)
+    for i, c := range counts {
+        if i == k {
+            break
+        }
+        result[i] = c[0]
+    }
+
+    return result
+}
+
+func oneCounter(nums []int) int {
+    result := int(0)
+    for _, num := range nums {
+        if num == 0 {
+            break
+        }
+        result++
+    }
+    return result
+}
+
+func main() {
+	// result:  [2,0,3]
+	// mat := [][]int{
+	// 	{1,1,0,0,0},
+	// 	{1,1,1,1,0},
+ 	// 	{1,0,0,0,0},
+ 	// 	{1,1,0,0,0},
+ 	// 	{1,1,1,1,1},
+	// } 
+	// k := int(3)
+
+	// result: [0,2]
+	mat := [][]int{
+		{1,0,0,0},
+		{1,1,1,1},
+ 		{1,0,0,0},
+ 		{1,0,0,0},
+	} 
+	k := int(2)
+
+	// result: 
+	// mat := [][]int{}
+	// k := int(0)
+
+	result := kWeakestRows(mat, k)
+	fmt.Printf("result = %v\n", result)
+}
+
