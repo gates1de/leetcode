c9cad0c20d40b3e2a71819f5f55fbc64088ca079
gates1de <g031i043@gmail.com>
2023-09-19 23:41:06 +0900

    2023-sep-week4-287: Accepted

---
+++ 2023/sep/week4/287/main.go
@@ -0,0 +1,32 @@
+package main
+import (
+	"fmt"
+	"sort"
+)
+
+func findDuplicate(nums []int) int {
+    sort.Ints(nums)
+    pre := nums[0]
+    for _, num := range nums[1:] {
+        if pre == num {
+            return num
+        }
+        pre = num
+    }
+    return -1
+}
+
+func main() {
+	// result: 2
+	// nums := []int{1,3,4,2,2}
+
+	// result: 3
+	nums := []int{3,1,3,4,2}
+
+	// result: 
+	// nums := []int{}
+
+	result := findDuplicate(nums)
+	fmt.Printf("result = %v\n", result)
+}
+
