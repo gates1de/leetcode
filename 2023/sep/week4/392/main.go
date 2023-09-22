32c93f65bb664697785362d2435cb8b483be890e
gates1de <g031i043@gmail.com>
2023-09-22 23:42:27 +0900

    2023-sep-week4-392: Accepted

---
+++ 2023/sep/week4/392/main.go
@@ -0,0 +1,47 @@
+package main
+import (
+	"fmt"
+)
+
+func isSubsequence(s string, t string) bool {
+    if len(s) > len(t) {
+        return false
+    } else if len(s) == 0 {
+        return true
+    } else if len(t) == 0 {
+        return false
+    } else if len(s) == len(t) {
+        return s == t
+    }
+
+    i := int(0)
+    for _, tChar := range t {
+        sChar := s[i]
+        if rune(sChar) == tChar {
+            i++
+        }
+        if i == len(s) {
+            return true
+        }
+    }
+
+    return false
+}
+
+func main() {
+	// result: true
+	// s := "abc"
+	// t := "ahbgdc"
+
+	// result: false
+	s := "axc"
+	t := "ahbgdc"
+
+	// result: 
+	// s := ""
+	// t := ""
+
+	result := isSubsequence(s, t)
+	fmt.Printf("result = %v\n", result)
+}
+
