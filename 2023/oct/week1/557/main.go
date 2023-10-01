b8ed275cbc0abfe0dfb11e46cb70b887277501c4
gates1de <g031i043@gmail.com>
2023-10-01 09:14:08 +0900

    2023-oct-week1-557: Accepted

---
+++ 2023/oct/week1/557/main.go
@@ -0,0 +1,42 @@
+package main
+import (
+	"fmt"
+)
+
+func reverseWords(s string) string {
+    startIndex := int(0)
+    result := ""
+    s += " "
+    for i, char := range s {
+        if char != rune(' ') {
+            continue
+        }
+
+        reversedChars := make([]byte, i - startIndex)
+        index := int(0)
+        for j := i - 1; j >= startIndex; j-- {
+            reversedChars[index] = s[j]
+            index++
+        }
+
+        result += string(reversedChars) + " "
+        startIndex = i + 1
+    }
+
+    return result[:len(result) - 1]
+}
+
+func main() {
+	// result: "s'teL ekat edoCteeL tsetnoc"
+	// s := "Let's take LeetCode contest"
+
+	// result: "doG gniD"
+	s := "God Ding"
+
+	// result: ""
+	// s := ""
+
+	result := reverseWords(s)
+	fmt.Printf("result = %v\n", result)
+}
+
