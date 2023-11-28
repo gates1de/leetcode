f73d3d80a361a6899a1093023629f1e0941e7049
gates1de <g031i043@gmail.com>
2023-11-28 10:39:23 +0900

    2023-dev-week1-2147: Accepted

---
+++ 2023/dec/week1/2147/main.go
@@ -0,0 +1,41 @@
+package main
+import (
+	"fmt"
+)
+
+const modulo = int(1e9 + 7)
+
+func numberOfWays(corridor string) int {
+	zero := int(0)
+	one := int(0)
+	two := int(1)
+
+	for _, char := range corridor {
+		if char == 'S' {
+			zero = one
+			one, two = two, one
+		} else {
+			two = (two + zero) % modulo
+		}
+	}
+
+	return zero
+}
+
+func main() {
+	// result: 3
+	// corridor := "SSPPSPS"
+
+	// result: 1
+	// corridor := "PPSPSP"
+
+	// result: 0
+	corridor := "S"
+
+	// result: 
+	// corridor := ""
+
+	result := numberOfWays(corridor)
+	fmt.Printf("result = %v\n", result)
+}
+
