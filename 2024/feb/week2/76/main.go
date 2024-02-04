bf643b77db1fc2255c40fad4fabf7507b138fd81
gates1de <g031i043@gmail.com>
2024-02-04 09:22:20 +0900

    2024-feb-week2-76: Accepted

---
+++ 2024/feb/week2/76/main.go
@@ -0,0 +1,74 @@
+package main
+import (
+	"fmt"
+	"math"
+)
+
+func minWindow(s string, t string) string {
+    if len(s) < len(t) {
+        return ""
+    }
+
+    m := map[byte]int{}
+
+    for _, v := range []byte(t) {
+        m[v]++
+    }
+
+    start := int(0)
+    end := int(0)
+    index := int(0)
+    counter := len(t)
+    minLen := math.MaxInt32
+
+    for end < len(s) {
+        if m[s[end]] > 0 {
+            counter--
+        }
+
+        m[s[end]]--
+        end++
+
+        for counter == 0 {
+            if minLen > end - start {
+                minLen = end - start
+                index = start
+            }
+
+            m[s[start]]++
+            if m[s[start]] == 1 {
+                counter++
+            }
+
+            start++
+        }
+    }
+
+    if minLen == math.MaxInt32 {
+        return ""
+    }
+
+    return s[index:index + minLen]
+}
+
+func main() {
+	// result: "BANC"
+	// s := "ADOBECODEBANC"
+	// t := "ABC"
+
+	// result: "a"
+	// s := "a"
+	// t := "a"
+
+	// result: ""
+	s := "a"
+	t := "aa"
+
+	// result: ""
+	// s := ""
+	// t := ""
+
+	result := minWindow(s, t)
+	fmt.Printf("result = %v\n", result)
+}
+
