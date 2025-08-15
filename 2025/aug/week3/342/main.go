c401a0654cffb39a6ce1004aeab5e82c073e94c5
gates1de <g031i043@gmail.com>
2025-08-15 09:41:36 +0900

    2025-aug-week3-342: Accepted

---
+++ 2025/aug/week3/342/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func isPowerOfFour(n int) bool {
+    m := make(map[int]bool)
+    for i := 1; i < math.MaxInt32; i *= 4 {
+        m[i] = true
+    }
+    return m[n]
+}
+
+func main() {
+	// result: true
+	// n := int(16)
+
+	// result: false
+	// n := int(5)
+
+	// result: true
+	n := int(1)
+
+	// result: 
+	// n := int(0)
+
+	result := isPowerOfFour(n)
+	fmt.Printf("result = %v\n", result)
+}
+
