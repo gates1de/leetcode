02a7b98d59c0b3e41d639240373cb279f39a55c5
gates1de <g031i043@gmail.com>
2026-01-24 10:42:12 +0900

    2026-jan-week4-1877: Accepted

---
+++ 2026/jan/week4/1877/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"sort"
+)
+
+func minPairSum(nums []int) int {
+	n := len(nums)
+	sort.Ints(nums)
+	result := math.MinInt32
+
+	for i, left := range nums[:n / 2] {
+		right := nums[n - i - 1]
+		sum := left + right
+		if result < sum {
+			result = sum
+		}
+	}
+
+	return result
+}
+
+func main() {
+	// result: 7
+	// nums := []int{3,5,2,3}
+
+	// result: 8
+	nums := []int{3,5,4,2,4,6}
+
+	// result: 
+	// nums := []int{}
+
+	result := minPairSum(nums)
+	fmt.Printf("result = %v\n", result)
+}
+
