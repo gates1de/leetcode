daefae88cae1d09d0053b0d9c31d5629b67f4338
gates1de <g031i043@gmail.com>
2026-06-19 09:42:38 +0900

    2026-june-week3-1732: Accepted

---
+++ 2026/june/week3/1732/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+func largestAltitude(gain []int) int {
+	result := int(0)
+	alt := int(0)
+	for _, num := range gain {
+		alt += num
+		result = max(result, alt)
+	}
+	return result
+}
+
+func main() {
+	// result: 1
+	// gain := []int{-5,1,5,0,-7}
+
+	// result: 0
+	gain := []int{-4,-3,-2,-1,4,3,2}
+
+	// result: 
+	// gain := []int{}
+
+	result := largestAltitude(gain)
+	fmt.Printf("result = %v\n", result)
+}
